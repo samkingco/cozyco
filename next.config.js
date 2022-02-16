2531d1b67a03bb1078cb674c85dd16cc403b9437
Sam King <mail@samking.co>
2022-02-16 00:53:35 +0000

    Maybe this fixes styled components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    styledComponents: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
